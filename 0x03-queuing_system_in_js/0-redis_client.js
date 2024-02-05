099362b6a7ee83e0619c06864497425fb46d221a
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-02-05 21:40:18 +0100

    [Feat] Node Redis client and basic operations

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,9 @@
 import { createClient } from 'redis';
 
-const client  =  createClient();
+const client = createClient();
 
-client.on(
-    'connect', () => {
-	console.log('Redis client connected to the server');
-    }).on(
-    'error', (err) => {
-	console.log(`Redis client not connected to the server: ${err}`);
-    });
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+}).on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
