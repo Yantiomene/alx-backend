fe35f567d18154e1090c021a28a68f567ca3439e
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-02-06 16:58:24 +0100

    [Feat] Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,22 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+}).on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+async function publishMessage(message, time) {
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+    await delay(time)
+    console.log(`About to send ${message}`);
+    client.publish("holberton school channel", message);
+    
+}
+
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
